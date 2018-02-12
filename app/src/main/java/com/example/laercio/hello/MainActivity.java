1e9f342e38dbee6872886bb38960aeb2ba363438
Keepstudy <keepstudy00@gmail.com>
2018-02-12 02:43:04 -0200

    Added code that get values from a JSON of a web page

--- app/src/main/java/com/example/laercio/hello/MainActivity.java
+++ app/src/main/java/com/example/laercio/hello/MainActivity.java
@@ -13,51 +13,90 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    // Arrays that contaim de information in the news feed.
+    String[] feedFotoPerfil;
+    String[] feedNome;
+    String[] feedObjetivo;
+    String[] feedPostagemImagem;
+    String[] feedDataRefeicao;
+    String[] feedCalorias;
 
     ListView newsFeed;
-    String[] items;
+    String jsonData;
+
+    Connection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String url = "http://api.tecnonutri.com.br/api/v4/feed";
-
-        try {
-            final OkHttpClient client = new OkHttpClient();
-
-            final Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Response responses = client.newCall(request).execute();
-                        String jsonData = responses.body().string().toString();
-                        JSONObject Jobject = new JSONObject(jsonData);
-                        JSONArray Jarray = Jobject.getJSONArray("items");
-                        Log.d("printHEHE", (Jarray.length() + ""));
-                        for (int i = 0; i < Jarray.length(); i++) {
-                            JSONObject object = Jarray.getJSONObject(i);
-                            Log.d("printHEHE", object.get("profile").toString());
-                        }
-                    } catch (Exception e) {
-                        Log.d("printHEHE","Nao deu nao");
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        } catch (Exception e) {
-            Log.d("HelloWorld", "deu nao 2");
-        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://api.tecnonutri.com.br/api/v4/feed";
+        JSONArray jsonArray;
+
+        newsFeed = (ListView) findViewById(R.id.newsFeed);
+        connection = new Connection();
+        connection.setUrl(url);
+
+        try {
+            jsonData = connection.getJSONData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// Class to make connection with the database in the url
+// This class has the potential to return a JSONArray
+class Connection{
+    String url;
+    String jsonData;
+
+    public String getJSONData() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Future<String> future = executor.submit(new Callable<String>(){
+            @Override
+            public String call(){
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
+
+                    Response responses = client.newCall(request).execute();
+                    String jsonData = responses.body().string().toString();
+
+                    return jsonData;
+
+                    /*JSONArray jsonArray = jsonObject[0].getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                    }*/
+                } catch (Exception e) {
+                    Log.d("debugPrint","Cant get string");
+                    return "";
+                }
+            }
+        });
+        jsonData = future.get();
+        future.isDone();
+        return jsonData;
+    }
+
+    public void setUrl(String url){
+        this.url = url;
     }
 }
